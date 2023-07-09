8b76793af1f76f7652e4e197ef7916ab86a7efd7
Lucas <lucasocabraldev17@gmail.com>
2023-07-09 12:36:32 -0300

    add layout

--- app/src/main/java/com/example/androidwidgetexample/MainActivity.kt
+++ app/src/main/java/com/example/androidwidgetexample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.androidwidgetexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.androidwidgetexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.largeButtonWater.setOnClickListener {
+            binding.lottieAnimationView.playAnimation()
+            binding.totalResult.text = getString(R.string.total_result, QUANTITY_LARGE_WATER)
+        }
+    }
+
+    companion object {
+        private const val QUANTITY_SMALL_WATER = 200
+        private const val QUANTITY_MEDIUM_WATER = 400
+        private const val QUANTITY_LARGE_WATER = 600
     }
 }
